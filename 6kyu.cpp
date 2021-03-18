0946e4251383d048e354c4505e1b6091cc44f85f
Fitri <fitrirozi1@gmail.com>
2021-03-17 23:23:43 -0500

    bouncing balls 6kyu completed in a clever way

---
+++ 6kyu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <typeinfo>
+
+using namespace std;
+
+class Bouncingball
+{
+public:
+    static int bouncingBall(double h, double bounce, double window)
+    {
+        // base case
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h)
+            return -1;
+        return bouncingBall(h * bounce, bounce, window) + 2;
+    }
+};
+
+int main()
+{
+    Bouncingball b;
+    cout << b.bouncingBall(3, 0.66, 1.5);
+}
