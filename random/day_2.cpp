9fe8fc54214d6f5d74cacc93685b9619eb263bbd
Fitri <fitrirozi1@gmail.com>
2021-03-19 00:41:44 -0500

    return odd number | clever way

--- random/day_2.cpp
+++ random/day_2.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
 int stray(vector<int> numbers)
 {
-    for (int i = 0; i < numbers.size() - 1; i++)
-        if (numbers[i] != numbers[i - 1] && numbers[i + 1])
-            return numbers[i];
+    map<int, int> counters;
+    for (int i = 0; i < numbers.size(); i++)
+        counters[numbers[i]] ? counters[numbers[i]] = 1 : counters[numbers[i]] += 1;
 
-    return 0;
+    for (auto item : counters)
+        if (item.second == 1)
+            return item.first;
+    return -1;
+}
+
+// However, this method is inefficient because sorting needs to go through the whole array at least once
+int stray_clever(vector<int> numbers)
+{
+    sort(numbers.begin(), numbers.end());
+    return (numbers[0] != numbers[1]) ? numbers.front() : numbers.back();
+}
+
+int stray_clever1(vector<int> numbers)
+{
+    int res = 0;
+    for (int n : numbers)
+        res ^= n;
+    return res;
 }
 
 int main()
 {
-    stray(vector<int>{1, 1, 2});
+    cout << stray_clever(vector<int>{3, 2, 2});
 }
