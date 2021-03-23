d5b98c5e4a40d988f7dfb9d02553a8d51700c9bb
Fitri <fitrirozi1@gmail.com>
2021-03-22 23:25:20 -0500

    solved two leetcode stack questions

---
+++ leetcode/backspace_string_compare.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string backspace(string s)
+    {
+        string strStack;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (i < s.size())
+            {
+                // If the stack is not empty and the program spots a '#' in s string, then pop the stack
+                if (s[i] == '#')
+                {
+                    if (!strStack.size())
+                        strStack.pop_back();
+                }
+                // Push a char into the string stack
+                else
+                    strStack.push_back(s[i]);
+            }
+        }
+        return strStack;
+    }
+    bool backspaceCompare(string S, string T)
+    {
+        // Returns true if the S string matches with the T string
+        return backspace(S) == backspace(T);
+    }
+};
+
+int main()
+{
+    Solution solve;
+    cout << solve.backspaceCompare("y#fo##f", "y#f#o##f");
+}
---
+++ leetcode/validate_stack_sequences.cpp
@@ -0,0 +1,43 @@
+// # 946
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool validateStackSequences(vector<int> pushed, vector<int> popped)
+    {
+        stack<int> st;
+        int i = 0;
+        // Iterate through the values of pushed
+        for (auto val : pushed)
+        {
+            // iterate until we see that value of pushed matches with the first element of popped
+            if (val == popped[i])
+            {
+                ++i;
+                // As long as the stack is not empty and the top of the stack matches with the current popped element
+                // then pop the  stack
+                while (!st.empty() && st.top() == popped[i])
+                {
+                    st.pop();
+                    i++;
+                }
+            }
+            // If there's no match, then push the val into the stack
+            else
+                st.push(val);
+        }
+        // Empty stack indicates that the sequences are valid
+        return st.empty();
+    }
+};
+
+int main()
+{
+    Solution answer;
+    cout << answer.validateStackSequences({1, 2, 3, 4, 5}, {4, 3, 5, 1, 2});
+}
